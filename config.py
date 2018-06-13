18a0d12048ab9d76bee6e7a16844affa19321884
Tim Miller <Tim@TimRCM.com>
2018-06-12 21:12:18 -0600

    Adding some config params

---
+++ config.py
@@ -0,0 +1,14 @@
+# Config parameters to change the game up a bit. 
+# Most of these should be treated as boolean values (0, or 1).
+
+# Enable or disable logging of game output 
+logging = 1
+
+# Ensure that your team is never changed
+stuck_team = 0
+
+# Guarantees that the bomb goes off when planted
+bomb_guaranteed = 0
+
+# Set your primary weapon (AK/M4) ammo amount
+ammo = 30
